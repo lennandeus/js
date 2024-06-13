b5a8f972cd66cf87d8281d6a6e628a173084c4c1
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2024-06-13 10:12:03 -0300

    teste

--- teste01/js/script.js
+++ teste01/js/script.js
@@ -22,7 +22,39 @@ let texto02 = null; // Vazio
 let num1 = 5
 let num2 = 3
 
-console.log(num1 + num2)
-console.log(num1 - num2)
-console.log(num1 * num2)
-console.log(num1 / num2)
+console.log(num1 + num2) // Adição
+console.log(num1 - num2) // Subtração
+console.log(num1 * num2) // Multiplicação
+console.log(num1 / num2) // Divisão
+console.log(num1 ** num2) // Potência
+console.log(num1 % num2) // Modulo
+
+// Operadores de comparação
+
+console.log("5" == 5) // igual
+console.log(5 == 5) // idêntico
+console.log(5 > 7) // falso, maior que
+console.log(5 < 7) // true, menor que
+console.log(5 <= 5) // menor ou igual que
+console.log(5>=5) // falso, igual ou maior que
+console.log(5 != 5) // falso, diferente
+console.log(5!==5) // falso, não idêntico
+
+// Operadores lógicos
+
+// E &&
+// ou ||
+// não !
+
+let a = 5
+let b = 10
+console.log(a > 0 && b > 5) // true
+console.log(a > 0 || b > 15) // true
+console.log(!b)
+console.log(a > 3 && b > -50)
+
+let resposta = confirm("O Palmeiras tem Mundial?")
+console.log(resposta)
+
+let resposta2 = confirm("Qual o seu nome?")
+console.log(resposta2)
