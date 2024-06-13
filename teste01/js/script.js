3cc4ecfb1abbd2f1be421300418d191283029f77
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2024-06-13 09:39:24 -0300

    primeiro commit

---
+++ teste01/js/script.js
@@ -0,0 +1,28 @@
+var nome = "Juan"
+let sobrenome = "Andrade"
+
+console.log(nome)
+console.log(sobrenome)
+
+
+if(true){
+    var nome2 = "Maria"
+}
+
+if(true){
+    var nome3 = "Matheus"
+}
+
+let numero = 1; // Inteiro
+let numero2 = 1.15 // Decimais
+let verdade = true // Booleano
+let texto // Indefinido
+let texto02 = null; // Vazio
+
+let num1 = 5
+let num2 = 3
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
